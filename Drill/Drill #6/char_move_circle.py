1cd4e49fdac62c4bfbbb3faf67bae1488728d76d
2021180033-LTH <mdk73000@naver.com>
2022-09-21 17:22:00 +0900

    Drill #6 submit
    circular move submit

--- Drill/Drill #6/char_move_circle.py
+++ Drill/Drill #6/char_move_circle.py
@@ -8,12 +8,12 @@ character = load_image('character.png')
 
 x = 0
 y = 0
-while (x < 2 * math.pi):
+while (x < 360):
     clear_canvas_now()
     grass.draw_now(400, 30)
-    character.draw_now(400 + x, 300 + y)
-    x = x + math.sin(1 / 360 * 2 * math.pi)
-    y = y + math.cos(1 / 360 * 2 * math.pi)
+    character.draw_now(400 + 4 * 60 * math.cos(x / 360 * 2 * math.pi), 330 + 4 * 60 * math.sin(y / 360 * 2 * math.pi))
+    x = x + 1
+    y = y + 1
     delay(0.01)
 
 close_canvas()
